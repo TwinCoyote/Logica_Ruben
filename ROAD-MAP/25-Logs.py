744b7b47451f59456e37c94bbcf70d356ca39c71
LRreyna <ruben.reyna@grupodimakers.com>
2026-05-26 12:17:50 -0600

    Nuevo tema RoadMap

--- ROAD-MAP/25-Logs.py
+++ ROAD-MAP/25-Logs.py
@@ -14,6 +14,7 @@
 #  * tarea ejecutada (a tu elección).
 #  * Utiliza el log para visualizar el tiempo de ejecución de cada tarea.
 
+import time
 import logging
 
 logging.basicConfig(
@@ -28,15 +29,56 @@ logging.basicConfig(
 # logging.error("Error de programa")
 
 
+# def tiempo(funcion):
+#     def wrapper(*args, **kwargs):
+#         print("===========================================")
+#         inicio = time.time()
+#         resultado = funcion(*args, **kwargs)
+#         final = time.time()
+#         print(final - inicio)
+#         print("===========================================")
+#         return resultado
+
+#     return wrapper
+
+
 actividades = {"Ruben": {"tarea": "Limpiar", "descripcion": "Se deberan de limpiar los trastes"},
                "Carolina": {"tarea": "Arreglar", "descripcion": "Se debera de arreglar la lavadora"}}
 
 
 def add(persona: str, tarea: str, text: str):
-    actividades[f"{persona}"]["tarea"] = f"{tarea}"
-    actividades[f"{persona}"]["descripcion"] = f"{text}"
-    logging.info("Se agrego la tarea con exito!")
+    inicio = time.time()
+    actividades[persona] = {"tarea": tarea, "descripcion": text, }
+    final = time.time()
+    tiempo = final - inicio
+    logging.info(f"Se agrego la tarea con exito! en {tiempo}")
 
 
-add("Ruben", "barrer", "Solo sera barrer la banqueta")
-print(actividades)
+def dele(persona: str):
+    inicio = time.time()
+    del actividades[f"{persona}"]
+    final = time.time()
+    logging.warning(
+        f"El usuario {persona} se ha quedado sin actvidad. se completo en {final-inicio} S.")
+
+
+def show():
+    inicio = time.time()
+    for i in actividades:
+        print(f"{actividades[i]["tarea"]} : {actividades[i]["descripcion"]}")
+    final = time.time()
+    t = round((final-inicio), 6)
+    logging.info(f"Se listaron las actividades. en {t} S.")
+
+
+# add("Ruben", "barrer", "Solo sera barrer la banqueta")
+
+add("Jorge", "Instruir", "Instruir en el levantamiento de pesas")
+print("\n")
+show()
+print("\n")
+dele("Jorge")
+print("\n")
+show()
+# print(actividades)
+# print(dele("Lalo"))
