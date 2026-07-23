d4747705e8d1a7977644cdea67659bbca97fcb72
TwinCoyote <twincoyote0732@gmail.com>
2026-07-23 09:10:05 -0600

    refactor: separate challenge repository from create module

--- reto_cli/core/create.py
+++ reto_cli/core/create.py
@@ -1,6 +1,8 @@
 
+
 import os
 import json
+import sys
 
 
 ROOT_DIR = os.path.abspath(os.path.join(
@@ -10,40 +12,43 @@ if ROOT_DIR not in sys.path:
 
 from repository.challenge_repository import find_challenge
 
-def limpiar_nombre_archivo(nombre: str) -> str:
+def clean_file_name(nombre: str) -> str:
     """Elimina caracteres inválidos para nombres de archivo en Windows."""
-    caracteres_invalidos = '<>:"/\\|?*'
-    for caracter in caracteres_invalidos:
-        nombre = nombre.replace(caracter, "")
+    valid_characters = '<>:"/\\|?*'
+    for character in valid_characters:
+        nombre = nombre.replace(character, "")
     return nombre.strip()
 
 
-def create_challenge_file(numero: int) -> str:
-    """Busca el reto en el Json y entrega un archivo con toda la info"""
-    try:
-        carpeta_destino = "Ejercicios_Logica"
-        if not os.path.exists(carpeta_destino):
-            os.makedirs(carpeta_destino, exist_ok=True)
-            numero = str(numero)
-            data = find_challenge(numero)
-            nombre_archivo = limpiar_nombre_archivo(f"{data.get("number")}_{data.get("name")}.py")
-            ruta_completa = os.path.join(carpeta_destino, nombre_archivo)
-            if os.path.exists(ruta_completa):
-                return "Ya hay un archivo asi creado"
-            with open(ruta_completa, "w", encoding="utf-8") as archivo:
-                # Título del archivo en docstring
-                archivo.write(f"'''{data.get("name").replace("_", " ")}'''\t")
-                # Dificultad como comentario en el formato solicitado
-                if data.get("dificulty") == "Dificil":
-                    archivo.write(f"#! Dificultad: {data.get("dificulty")}\n")
-                else:
-                    archivo.write(f"#? Dificultad: {data.get("dificulty")}\n")
-                archivo.write(
-                    "# pylint: disable = E0001, C0103, C0114,C0115, C0116,W0622,W3101\n")
-                # No presentar el primer y ulitmo caracter porque es un '#' vacio
-                archivo.write(f"#{data.get("description")[1:-1]}")
-        return "Exito", "Archivo creado correctamente", ruta_completa
-    except KeyError:
-        return "Error", numero
-    except NameError:
-        return "Error", numero
+def create_challenge_file(number: int) -> str:
+    """# Create a file with the json data"""
+
+    # TODO: Migrate all constants to new file '.env' or 'constants' or something like that idk
+    destination_folder = "Ejercicios_Logica"
+    if not os.path.exists(destination_folder):
+        os.makedirs(destination_folder, exist_ok=True)
+    data = find_challenge(number)
+    if not data:
+        return "Error", f"Not data in challenge {number}.", None
+    file_name = clean_file_name(
+        f"{data.get("number")}_{data.get("name")}.py")
+    path = os.path.join(destination_folder, file_name)
+    if os.path.exists(path):
+        return "already exist file"
+    # Título del archivo en docstring
+    with open(path, "w", encoding="utf-8") as file:
+        file.write(f"'''{data.get("name").replace("_", " ")}'''\t")
+        # Dificultad como comentario en el formato solicitado
+        if data.get("dificulty") == "Dificil":
+            file.write(f"#! Dificulty: {data.get("dificulty")}\n")
+        else:
+            file.write(f"#? Dificulty: {data.get("dificulty")}\n")
+        file.write(
+            "# pylint: disable = E0001, C0103, C0114,C0115, C0116,W0622,W3101\n")
+        # No presentar el primer y ulitmo character porque es un '#' vacio
+        file.write(f"#{data.get("description")[1:-1]}")
+    return "success", "File created successfully", path
+
+
+
+print(create_challenge_file(99))
