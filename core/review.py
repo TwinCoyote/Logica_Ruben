f73e0cc7703a77aa096886a347b034cde783bbf3
TwinCoyote <twincoyote0732@gmail.com>
2026-07-24 13:49:38 -0600

    implement review command in CLI and enhance review output formatting

--- core/review.py
+++ core/review.py
@@ -1,21 +1,25 @@
 # pylint: disable = E0401,C0403,W0611,C0413,C0412,C0114,W0511,E0602,W0718
 
 
+
 import sys
 from pathlib import Path
 from google.genai import errors
 
-ROOT_DIR = Path(__file__).resolve().parent.parent
+ROOT_DIR = Path(__file__).resolve().parent
 if str(ROOT_DIR) not in sys.path:
     sys.path.insert(0, str(ROOT_DIR))
 
+PARENT_DIR = Path(__file__).resolve().parent.parent
+if str(PARENT_DIR) not in sys.path:
+    sys.path.insert(0, str(PARENT_DIR))
 
-from ui.reporter import *
-from repository.file_repository import find_challenge_file
-from extract_solution import solution_scrapper
-from build_prompt import build_prompt
-from ai_client import review_code
 from parse_response import json_to_dict
+from ai_client import review_code
+from build_prompt import build_prompt
+from extract_solution import solution_scrapper
+from repository.file_repository import find_challenge_file
+from ui.reporter import *
 
 try:
     from repository.challenge_repository import find_challenge
@@ -40,15 +44,15 @@ def review_challenge(challenge_number: int):
 
         review = review_code(prompt)
 
-        convert = json_to_dict(review)
+        analysis = json_to_dict(review)
 
-        # TODO: reporter()
+        show_info = show_review(analysis)
 
-        return convert
+        return show_info
     except errors.APIError as e:
         show_general_error(e)
     except Exception as e:
         show_general_error(e)
 
 
-print(review_challenge(27))
+# print(review_challenge(27))
