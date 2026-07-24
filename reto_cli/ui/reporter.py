c03a1b01ee99929f842cfaa8af3cf41aefb19059
TwinCoyote <twincoyote0732@gmail.com>
2026-07-24 13:49:38 -0600

    implement review command in CLI and enhance review output formatting

--- reto_cli/ui/reporter.py
+++ reto_cli/ui/reporter.py
@@ -1,5 +1,5 @@
 import ast
-
+from textwrap import dedent
 
 # * Succes
 
@@ -35,3 +35,62 @@ def show_info(msg: str) -> None:
     '''Print a custom message like info'''
     print(f"{msg}...")
     return None
+
+
+# * Analysis
+
+def show_review(dic: dict):
+    """Print the review from Ai"""
+
+    top = f"""
+    =====================================
+                Review Report
+    =====================================
+
+⭐ Score:                    {dic.get("score")}
+
+✅ Correct:                  {dic.get('time_complexity')}
+
+💾 Space Complexity:         {dic.get("time_complexity")}
+
+📊 Code Quality:             {dic.get("code_quality")}
+
+📖 Readability:              {dic.get("readability")}
+
+🛠 Maintainability:           {dic.get("maintainability")}
+    """
+
+    strengths = "\n".join(f"✔ {x}" for x in dic.get("strengths", []))
+    bugs = "\n".join(f"✖ {x}" for x in dic.get("bugs", []))
+    suggestions = "\n".join(f"→  {x}" for x in dic.get("suggestions", []))
+
+    lists = f"""
+Strengths
+__________
+
+{strengths}
+
+Potential Bugs
+_______________
+
+{bugs}
+
+Suggestions
+-----------
+
+{suggestions}
+
+"""
+
+    output = f"""
+    {top}
+
+    {lists}
+    
+    
+    """
+
+    return output
+
+
+
